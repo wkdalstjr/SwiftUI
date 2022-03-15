c6c5b970d82b45711d1aca2ada37a308824ca1a0
wkdalstjr <wkdalstjr1218@naver.com>
2022-03-15 15:13:27 +0900

    GeometryProxy position

--- Layout_practice/Layout_practice/MyGeometryReaderVStack.swift
+++ Layout_practice/Layout_practice/MyGeometryReaderVStack.swift
@@ -15,9 +15,14 @@ struct MyGeometryReaderVStack: View {
     
     @State var index : Index = .one
     
+    //지오메트리 프록시를 매개변수로 가지고 CGPoint 를 반환하는 클로저
+    let centerPosition : (GeometryProxy) -> CGPoint = { proxy in
+        return CGPoint(x:proxy.frame(in: .local).midX, y: proxy.frame(in: .local).midY)
+    }
+    
     var body: some View {
         
-        GeometryReader { geometry in
+        GeometryReader { proxy in
             VStack {
                 
                 Button(action:{
@@ -33,51 +38,54 @@ struct MyGeometryReaderVStack: View {
                     Text("1")
                         .font(.largeTitle)
                         .fontWeight(.black)
-                        .frame(width: 100, height: geometry.size.height / 3)
+                        .frame(width: 100, height: proxy.size.height / 3)
                         .padding(.horizontal, self.index == .one ? 50 : 0)
                         .foregroundColor(.white)
                         .background(Color.red)
                 }
                 
-                Button(action:{
-                    // 버튼이 클릭되었을 때 로직
-                    print("2번이 클릭되었습니다.")
-                    
-                    withAnimation{
-                        self.index = .two
-                    }
-                    
-                }){
-                    // 버튼의 생김새
-                    Text("2")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                        .frame(width: 100, height: geometry.size.height / 3)
-                        .padding(.horizontal, self.index == .two ? 50 : 0)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-                }
-                
-                Button(action:{
-                    // 버튼이 클릭되었을 때 로직
-                    print("3번이 클릭되었습니다.")
-                    
-                    withAnimation{
-                        self.index = .three
-                    }
-                    
-                }){
-                    // 버튼의 생김새
-                    Text("3")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                        .frame(width: 100, height: geometry.size.height / 3)
-                        .padding(.horizontal, self.index == .three ? 50 : 0)
-                        .foregroundColor(.white)
-                        .background(Color.green)
-                }
+//                Button(action:{
+//                    // 버튼이 클릭되었을 때 로직
+//                    print("2번이 클릭되었습니다.")
+//
+//                    withAnimation{
+//                        self.index = .two
+//                    }
+//
+//                }){
+//                    // 버튼의 생김새
+//                    Text("2")
+//                        .font(.largeTitle)
+//                        .fontWeight(.black)
+//                        .frame(width: 100, height: proxy.size.height / 3)
+//                        .padding(.horizontal, self.index == .two ? 50 : 0)
+//                        .foregroundColor(.white)
+//                        .background(Color.blue)
+//                }
+//
+//                Button(action:{
+//                    // 버튼이 클릭되었을 때 로직
+//                    print("3번이 클릭되었습니다.")
+//
+//                    withAnimation{
+//                        self.index = .three
+//                    }
+//
+//                }){
+//                    // 버튼의 생김새
+//                    Text("3")
+//                        .font(.largeTitle)
+//                        .fontWeight(.black)
+//                        .frame(width: 100, height: proxy.size.height / 3)
+//                        .padding(.horizontal, self.index == .three ? 50 : 0)
+//                        .foregroundColor(.white)
+//                        .background(Color.green)
+//                }
                 
             }
+//            .position(CGPoint(x:proxy.frame(in: .local).midX, y: proxy.frame(in: .local).midY))
+            .position(centerPosition(proxy))
+            
         }
         .background(Color.yellow)
         .edgesIgnoringSafeArea(.all)
